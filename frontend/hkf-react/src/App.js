a9ed666d43dccc010a8a1bd4bc9e9cfacfda2902
Alanna423 <alanna.zhang423@gmail.com>
2023-03-07 17:51:20 -0500

    minor styling changes

--- frontend/hkf-react/src/App.js
+++ frontend/hkf-react/src/App.js
@@ -7,7 +7,13 @@ function App() {
   return (
     <div>
       <div className="app-body">
-        <h1>Human Kindness Foundation | Location Tracker</h1>
+        <div className="info">
+          <h1>Human Kindness Foundation | Location Tracker</h1>
+          <h3>Instructions:</h3>
+          <h5>1. Upload a file using the "Choose File" button. The format of this file should be xlsx.</h5>
+          <h5>2. Select "Find Addresses" to get each person's location.</h5>
+          <h5>3. Download the changed file with "Download New File".</h5>
+        </div>
         <ChooseFileComponent/> <br />
         <br />
       </div>

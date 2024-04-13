8b320d460fb0508269575aa7331b0ad0c9b4cd67
nsivaku <nsivaku@unc.edu>
2024-04-13 03:35:29 -0400

    update

--- frontend/hkf-react/src/components/ChooseFileComponent.js
+++ frontend/hkf-react/src/components/ChooseFileComponent.js
@@ -40,7 +40,7 @@ function ChooseFileComponent() {
     const formData = new FormData();
     formData.append('data', parsedSheet);
     setStatus("loading")
-    fetch('/input', {
+    fetch('https://hkf-production.up.railway.app/input', {
       method: "POST",
       body: formData
     }).then(res => res.json())
